6667e26e98157abdd0e954c1760a58fc5a1ff5cd
mwq <maoturing@qq.com>
2018-11-20 14:09:00 +0800

    Initial commit

---
+++ src/main/java/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by User on 2018/11/20.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+    }
+}
