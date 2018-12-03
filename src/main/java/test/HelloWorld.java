dff2360de1322f468bdf6b1ad83a8c4c9c43375d
mwq <maoturing@qq.com>
2018-12-03 14:21:19 +0800

    四种对象引用类型

--- src/main/java/test/HelloWorld.java
+++ src/main/java/test/HelloWorld.java
@@ -1,5 +1,11 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 /**
  * Created by User on 2018/11/20.
  */
@@ -7,5 +13,13 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("hello world");
+        List<String> list = new ArrayList<>();
+        list.add("aaa");
+        Predicate<String> p = s -> "aaa".equalsIgnoreCase(s);
+        list.removeIf(p.and(p));
+        list.forEach(s -> System.out.println(s));
+        Function<String, Integer> f = s -> s.length();
+        list.stream().map(f).collect(Collectors.toList());
+
     }
 }
